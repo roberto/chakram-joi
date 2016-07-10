f311175be2ee447915b1ee9e0619ae4972bb278b
Roberto Soares <roberto.tech@gmail.com>
2016-07-10 12:55:57 -0300

    Rewrite project using ES5

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const joi = require('joi');
+'use strict';
 
-const DEFAULT_OPTIONS = {
+var joi = require('joi');
+
+var DEFAULT_OPTIONS = {
   abortEarly: true,
   presence: 'required',
   allowUnknown: true
 };
 
-const joiMethod = function (respObj, schema, options = {}) {
-  const body = respObj.response.body;
-  const result = joi.validate(body, schema,
+var joiMethod = function joiMethod(respObj, schema, options) {
+  if (!options) options = {};
+
+  var body = respObj.response.body;
+  var result = joi.validate(body, schema,
     Object.assign({}, DEFAULT_OPTIONS, options));
 
   this.assert(!result.error,
-    `expected body to match Joi schema ${errorMessages(result.error)}`,
+    'expected body to match Joi schema ' + errorMessages(result.error),
     'expected body to not match Joi schema');
 };
 
-const errorMessages = (errors) => {
+var errorMessages = function errorMessages(errors) {
   if (!errors) return;
 
-  return errors.details.reduce((message, error) =>
-    `${message}\n-----\n error: ${error.message}.\n data path: ${error.path}`, '');
+  return errors.details.reduce(function (message, error) {
+    return message + '\n-----\n error: ' + error.message + '.\n data path: ' + error.path;
+  }, '');
 };
 
 module.exports = joiMethod;
--- test/index.js
+++ test/index.js
@@ -1,107 +1,102 @@
-const chai = require('chai');
-const chakram = require('chakram');
-const expect = chakram.expect;
-const joi = require('joi');
-const joiMethod = require('../index');
-const sinon = require('sinon');
-const sinonChai = require("sinon-chai");
+'use strict';
+
+var chai = require('chai');
+var chakram = require('chakram');
+var expect = chakram.expect;
+var joi = require('joi');
+var joiMethod = require('../index');
+var sinon = require('sinon');
+var sinonChai = require("sinon-chai");
 
 chai.use(sinonChai);
 
-describe('joiMethod', () => {
-  before(() => {
+describe('joiMethod', function () {
+  before(function () {
     chakram.addMethod('joi', joiMethod);
   });
 
-  const respObj = (obj) => ({
-    response: {
-      body: obj
-    }
-  });
-  const schema = joi.object().keys({
+  var respObj = function respObj(obj) {
+    return {
+      response: {
+        body: obj
+      }
+    };
+  };
+  var schema = joi.object().keys({
     name: joi.string()
   });
-  const validObject = {name: 'test'};
-  const invalidObject = {id: 'test'};
+  var validObject = { name: 'test' };
+  var invalidObject = { id: 'test' };
 
-  describe('positive assertion', () => {
-    it('passes when object is valid', () => {
+  describe('positive assertion', function () {
+    it('passes when object is valid', function () {
       expect(respObj(validObject)).to.joi(schema);
     });
 
-    describe('for invalid object', () => {
-      it('fails', () => {
-        expect(() => {
+    describe('for invalid object', function () {
+      it('fails', function () {
+        expect(function () {
           expect(respObj(invalidObject)).to.joi(schema);
         }).to.throw(chai.AssertionError, /expected body to match Joi schema/);
       });
 
-      it('adds error message indicating issue', () => {
-        expect(() => {
+      it('adds error message indicating issue', function () {
+        expect(function () {
           expect(respObj(invalidObject)).to.joi(schema);
         }).to.throw(chai.AssertionError, /error: "name" is required.\n data path: name/);
       });
     });
   });
 
-  describe('negative assertion', () => {
-    it('passes when object is invalid', () => {
+  describe('negative assertion', function () {
+    it('passes when object is invalid', function () {
       expect(respObj(invalidObject)).not.to.joi(schema);
     });
 
-    it('fails when object is valid', () => {
-      expect(() => {
+    it('fails when object is valid', function () {
+      expect(function () {
         expect(respObj(validObject)).not.to.joi(schema);
       }).to.throw(chai.AssertionError, /expected body to not match Joi schema/);
     });
   });
 
-  describe('options', () => {
-    let validate;
+  describe('options', function () {
+    var validate;
 
-    beforeEach(() => {
+    beforeEach(function () {
       validate = sinon.spy(joi, 'validate');
     });
 
-    afterEach(() => {
+    afterEach(function () {
       validate.restore();
     });
 
-    it('has about early, presence as required and allow unknown keys as default options', () => {
+    it('has about early, presence as required and allow unknown keys as default options', function () {
       expect(respObj(validObject)).to.joi(schema);
-      expect(validate).to.have.been.calledWith(
-        sinon.match.any,
-        sinon.match.any,
-        {
-          abortEarly: true,
-          allowUnknown: true,
-          presence: 'required'
-        });
+      expect(validate).to.have.been.calledWith(sinon.match.any, sinon.match.any, {
+        abortEarly: true,
+        allowUnknown: true,
+        presence: 'required'
+      });
     });
 
-    it('allows custom options', () => {
+    it('allows custom options', function () {
       expect(respObj(validObject)).to.joi(schema, { stripUnknown: true });
-      expect(validate).to.have.been.calledWith(
-        sinon.match.any,
-        sinon.match.any,
-        {
-          abortEarly: true,
-          allowUnknown: true,
-          presence: 'required',
-          stripUnknown: true
-        });
+      expect(validate).to.have.been.calledWith(sinon.match.any, sinon.match.any, {
+        abortEarly: true,
+        allowUnknown: true,
+        presence: 'required',
+        stripUnknown: true
+      });
     });
 
-    it('allows overriding default options', () => {
+    it('allows overriding default options', function () {
       expect(respObj(validObject)).to.joi(schema, { abortEarly: false });
-      expect(validate).to.have.been.calledWith(
-        sinon.match.any,
-        sinon.match.any,
-        {
-          abortEarly: false,
-          allowUnknown: true,
-          presence: 'required'
-        });
+      expect(validate).to.have.been.calledWith(sinon.match.any, sinon.match.any, {
+        abortEarly: false,
+        allowUnknown: true,
+        presence: 'required'
+      });
     });
   });
 });
